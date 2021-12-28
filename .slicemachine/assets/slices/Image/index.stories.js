cd474e61a4f7c584ea037465454ca61766a09a32
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-27 22:39:26 -0500

    Chore: Update mocks

--- .slicemachine/assets/slices/Image/index.stories.js
+++ .slicemachine/assets/slices/Image/index.stories.js
@@ -5,11 +5,11 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1541807084-5c52b6b3adef?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587840171670-8b850147754e?w=900&h=500&fit=crop"}},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
 
-export const _FullWidth = () => <MyComponent slice={{"variation":"fullWidth","name":"Full Width","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=900&h=500&fit=crop"}},"id":"_FullWidth"}} />
+export const _FullWidth = () => <MyComponent slice={{"variation":"fullWidth","name":"Full Width","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1600861194802-a2b11076bc51?w=900&h=500&fit=crop"}},"id":"_FullWidth"}} />
 _FullWidth.storyName = 'Full Width'
 
-export const _WithCaption = () => <MyComponent slice={{"variation":"withCaption","name":"With Caption","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1448932223592-d1fc686e76ea?w=900&h=500&fit=crop"},"caption":[{"type":"paragraph","text":"Nostrud ad consequat sunt irure exercitation Lorem veniam nulla sit ullamco.","spans":[]}]},"id":"_WithCaption"}} />
+export const _WithCaption = () => <MyComponent slice={{"variation":"withCaption","name":"With Caption","slice_type":"image","items":[],"primary":{"image":{"dimensions":{"width":900,"height":500},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1593642532973-d31b6557fa68?w=900&h=500&fit=crop"},"caption":[{"type":"paragraph","text":"Nisi do sunt id commodo culpa dolore. Ut ipsum id minim dolor non et et in anim aute duis enim ipsum est non. Anim sit veniam sunt nulla ipsum aliqua ullamco ullamco sunt proident exercitation ipsum pariatur.","spans":[]}]},"id":"_WithCaption"}} />
 _WithCaption.storyName = 'With Caption'
--- .slicemachine/assets/slices/RichText/index.stories.js
+++ .slicemachine/assets/slices/RichText/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"rich_text","items":[],"primary":{"text":[{"type":"paragraph","text":"Nulla non proident magna veniam magna.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"rich_text","items":[],"primary":{"text":[{"type":"paragraph","text":"Eu nulla eiusmod voluptate occaecat. Ullamco ipsum nisi nostrud ea nulla nisi.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
