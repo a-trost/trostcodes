d448c0656f5a1e2f8f2b99c06cddacd52f935c4b
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-28 11:08:14 -0500

    Add blockQuote Slice

---
+++ .slicemachine/assets/slices/BlockQuote/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/BlockQuote';
+
+export default {
+  title: 'slices/BlockQuote'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"block_quote","items":[],"primary":{"text":[{"type":"paragraph","text":"Minim quis Lorem do non ad ullamco exercitation consequat cupidatat. Commodo enim est et est nulla.","spans":[]}],"attribution":[{"type":"paragraph","text":"Mollit culpa exercitation incididunt nostrud in veniam eiusmod sunt.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
