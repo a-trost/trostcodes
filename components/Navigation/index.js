283da5d6ab6c7a05ffd3527291dcfe445d6433d7
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-28 23:00:00 -0500

    Begin to build out mobile nav (WIP)

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,46 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import { PrismicRichText } from "@prismicio/react";
 import Link from "@/components/Link";
+import { motion } from "framer-motion";
+import { HiX, HiMenu } from "react-icons/hi";
 
 export default function Navigation({ menu }) {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  console.log("toggleMobileNav", mobileNavOpen);
+
+  const toggleMobileNav = () => {
+    console.log("toggleMobileNav", mobileNavOpen);
+    setMobileNavOpen(!mobileNavOpen);
+  };
+
   const menuData = {
     title: menu.data.title,
     links: menu.data.menu_links,
   };
+
+  const hideNavItemsVariant = {
+    opened: {
+      opacity: 0,
+      y: "-100%",
+      transition: {
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+    closed: {
+      opacity: 1,
+      y: "0%",
+      transition: {
+        delay: 1.1,
+        duration: 0.5,
+        ease: "easeInOut",
+      },
+    },
+  };
+
   return (
-    <>
-      <nav className="layout-grid">
-        <div className="navigation">
-          <div className="navigation__title">
-            <Link document={{ url: "/", link_type: "Document" }}>
-              <PrismicRichText
-                field={menuData.title}
-                components={{
-                  paragraph: ({ children, key }) => (
-                    <React.Fragment key={key}>{children}</React.Fragment>
-                  ),
-                }}
-              />
-            </Link>
-          </div>
-          <ul className="navigation__list">
-            {menuData.links.map((link) => (
-              <li key={link.title}>
-                <Link document={link.link}>
-                  <PrismicRichText
-                    field={link.label}
-                    components={{
-                      paragraph: ({ children, key }) => (
-                        <React.Fragment key={key}>{children}</React.Fragment>
-                      ),
-                    }}
-                  />
-                </Link>
-              </li>
-            ))}
-          </ul>{" "}
+    <motion.nav
+      initial="closed"
+      animate={mobileNavOpen ? "opened" : "closed"}
+      className="layout-grid"
+    >
+      <div className="navigation">
+        <div className="navigation__title">
+          <Link document={{ url: "/", link_type: "Document" }}>
+            <PrismicRichText
+              field={menuData.title}
+              components={{
+                paragraph: ({ children, key }) => (
+                  <React.Fragment key={key}>{children}</React.Fragment>
+                ),
+              }}
+            />
+          </Link>
         </div>
-      </nav>
-    </>
+        <NavigationList
+          menu={menuData}
+          screen="desktop"
+          mobileNavOpen={mobileNavOpen}
+        />
+        <NavigationList
+          menu={menuData}
+          screen="mobile"
+          mobileNavOpen={mobileNavOpen}
+          setMobileNavOpen={setMobileNavOpen}
+        />
+        <motion.button
+          type="button"
+          variants={hideNavItemsVariant}
+          onClick={toggleMobileNav}
+        >
+          {mobileNavOpen ? <HiMenu /> : <HiX />}
+        </motion.button>
+      </div>
+    </motion.nav>
   );
 }
+
+const NavigationList = ({ menu, screen = "desktop" }) => {
+  return (
+    <ul className={`navigation__list navigation__list--${screen}`}>
+      {menu.links.map((link) => (
+        <li key={link.label[0].text + screen}>
+          <Link document={link.link}>
+            <PrismicRichText
+              field={link.label}
+              components={{
+                paragraph: ({ children, key }) => (
+                  <React.Fragment key={key}>{children}</React.Fragment>
+                ),
+              }}
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
