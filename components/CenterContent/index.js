847564f9baac61ec461fcee9fe8d65e2f6f30951
Alex Trost <19617280+a-trost@users.noreply.github.com>
2022-02-11 23:27:46 -0500

    Add center content component

---
+++ components/CenterContent/index.js
@@ -0,0 +1,11 @@
+export default function CenterContent({
+  maxWidth = "400px",
+  children,
+  ...props
+}) {
+  return (
+    <div style={{ maxWidth: maxWidth, margin: "1rem auto" }} {...props}>
+      {children}
+    </div>
+  );
+}
