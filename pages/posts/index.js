ab11a85ad154f664a16b199bdf7cf8b86023aa93
Alex Trost <19617280+a-trost@users.noreply.github.com>
2022-02-11 23:28:15 -0500

    Only pull in needed data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -6,14 +6,9 @@ import Layout from "@components/Layout";
 import Head from "next/head";
 
 export default function Posts({ posts, page, menu, footer, ...props }) {
-  const formattedPosts = posts.map((post) => {
-    return {
-      ...post,
-      url: post.url,
-      tags: post.frontMatter.tags || [],
-      title: post.frontMatter.title,
-      blurb: post.frontMatter.desc,
-    };
+  // Sort posts by date "YYYY-MM-DD"
+  const sortedPosts = posts.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date);
   });
 
   return (
@@ -31,7 +26,7 @@ export default function Posts({ posts, page, menu, footer, ...props }) {
           <SliceZone {...page.data} resolver={resolver} />
         )}
         <ul className="card-grid">
-          {formattedPosts.map((post) => (
+          {sortedPosts.map((post) => (
             <li className="card" key={post.url}>
               <div>
                 <a href={`${post.url}`} key={post.url} className="card__title">
@@ -57,10 +52,21 @@ export default function Posts({ posts, page, menu, footer, ...props }) {
 export async function getStaticProps() {
   const { menu, footer } = await getFooterAndMenu();
   const posts = await getAllNodes("post");
+
+  const formattedPosts = posts.map((post) => {
+    return {
+      url: post.url,
+      tags: post.frontMatter.tags || [],
+      title: post.frontMatter.title,
+      blurb: post.frontMatter.desc,
+      date: post.frontMatter.date,
+    };
+  });
+
   const page = await getPageByUID("posts");
   return {
     props: {
-      posts,
+      posts: formattedPosts,
       page,
       menu,
       footer,
